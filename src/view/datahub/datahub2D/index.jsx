70b7350b1f3f0db04018b990a1eb6e1ca83ecd08
zhao.jing <zhao.jing@dhwork.cn>
2021-03-09 14:48:31 +0800

    2D主控台

--- src/view/datahub/datahub2D/index.jsx
+++ src/view/datahub/datahub2D/index.jsx
@@ -3,15 +3,23 @@ import React, { useEffect, useState } from 'react';
 import { Button } from 'antd';
 import { Map, View } from 'ol';
 import * as Proj from 'ol/proj';
-import { Tile as TileLayer } from 'ol/layer'; // 图层
-import { XYZ } from 'ol/source';
+import TileLayer from 'ol/layer/Tile'; // 图层
+import XYZ from 'ol/source/XYZ';
 // import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'; // 图层
 // import { XYZ, Vector as VectorSource } from 'ol/source';
 
 const DataHub2D = ({ setSelect2DOr3D }) => {
     const [map, setMap] = useState(null); // map对象
     const [view, setView] = useState(null); // view对象
-
+    function amapFn() {
+        let Amap = new TileLayer({
+            source: new XYZ({
+                maxZoom: 20,
+                url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&scl=1&size=1&style=7&x={x}&y={y}&z={z}'
+            })
+        });
+        return Amap;
+    }
     useEffect(() => {
 
     }, [map]);
@@ -22,12 +30,7 @@ const DataHub2D = ({ setSelect2DOr3D }) => {
             // 创建实例
             const _map = new Map({
                 target: 'map',
-                layers: [new TileLayer({
-                    source: new XYZ({
-                        maxZoom: 20,
-                        url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&scl=1&size=1&style=7&x={x}&y={y}&z={z}'
-                    })
-                })],
+                layers: [amapFn()],
                 view: view
             });
             setMap(_map);
@@ -37,8 +40,8 @@ const DataHub2D = ({ setSelect2DOr3D }) => {
     useEffect(() => {
         // View用于创建2D视图
         const viewObj = new View({
-            center: Proj.transform([103.9271879196, 30.7462617980], 'EPSG:4326', 'EPSG:3867'),
-            zoom: 5
+            center: Proj.transform([103.9271879196, 30.7462617980], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 12
         });
         setView(viewObj);
     }, []);
